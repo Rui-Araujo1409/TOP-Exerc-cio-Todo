46e24be24acdd6db87625f2513c65f0c671b6790
Rui-Araujo1409 <rui1409@gmail.com>
2025-11-15 15:59:01 +0000

    criar fx adicionar e retirar tarefas

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 class Projecto {
-  constructor(título, utilizadores, todos){
+  constructor(título, utilizadores){
     this.título = título;
   this.utilizador = utilizadores;
+    this.todos = [];
   }
+  
+  adicionarTodo(todo) {this.todos.push(todo)}
   
 }
 
-class ListaTarefasTodo {
-  constructor(tarefas){
-    this.tarefas = tarefas;
+class TarefasTodo {
+  constructor(){
+    this.tarefas = [];
+  }
+  
+  adicionarTarefa(item) {this.tarefas.push(item)}
+  
+  retirarTarefa(item) {
+    let index = this.tarefas.findIndex(elemento => elemento == item);
+  this.tarefas.splice(index,1);
   }
 }
 
 class Todo extends Projecto {
-  constructor(título, utilizador, descrição, dataTérmino, prioridade, notas, tarefas) {
+  constructor(título, utilizador, descrição, dataTérmino, prioridade, activo, notas, tarefas) {
     super(título, utilizador);
   this.descrição = descrição;
   this.dataTérmino = dataTérmino;
   this.prioridade = prioridade;
+    this.activo = activo;
   this.notas = notas;
     this.tarefas = tarefas;
   }
  
 }
 
-const projecto1 = new Projecto("Projecto 1", ["Rui Araújo", "Joaquim Araújo", "Ana Araújo"], []);
+const tarefasTodo1 = new TarefasTodo();
+const tarefasTodo2 = new TarefasTodo();
 
-const todo1 = new Todo("Todo 1", "Rui Araújo", "descrição", "data", 1, "notas", []);
 
-const todo2 = new Todo("Todo 2", "Joaquim Araújo", "descrição", "data", 1, "notas", []);
+const projecto1 = new Projecto("Projecto 1", ["Rui Araújo", "Joaquim Araújo", "Ana Araújo"]);
 
-const listaTarefasTodo1 = new ListaTarefasTodo(["tarefa1", "tarefa2", "tarefa3"]);
+const todo1 = new Todo("Todo 1", "Rui Araújo", "descrição", "data", 1, true, "notas", tarefasTodo1);
+
+const todo2 = new Todo("Todo 2", "Joaquim Araújo", "descrição", "data", 0, false, "notas", tarefasTodo2);
+
 
 
 console.log(projecto1);
 console.log(todo1);
 console.log(todo2);
 
-todo1.tarefas = listaTarefasTodo1;
-console.log(todo1.tarefas.tarefas[0]);
+
 
 const obj1 = {
   título: projecto1.título,
@@ -46,4 +59,14 @@ const obj1 = {
   todos: todo1
 }
 
-console.log(obj1);
+tarefasTodo1.adicionarTarefa("tarefa1");
+tarefasTodo1.adicionarTarefa("tarefa2");
+tarefasTodo1.adicionarTarefa("tarefa3");
+tarefasTodo1.adicionarTarefa("tarefa4");
+console.log(tarefasTodo1);
+tarefasTodo1.retirarTarefa("tarefa2");
+console.log(tarefasTodo1);
+console.log(todo1);
+
+
+
